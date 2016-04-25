04d58cd96ef79cdf1043cf1f5b399603dc1d8c8a
pmario26 <pmario26@gmail.com>
2016-04-25 20:45:35 -0300

    Added files via upload

---
+++ main01.ino
@@ -0,0 +1,18 @@
+
+const int Um_Temp = A0;
+const int Solo_R = A1;
+const int Solo_G = A2;
+const int Solo_N = A3;
+float temperature;
+int B=3975;                  //B value of the thermistor
+float resistance;
+float umidade_R;
+float Umidade_G;
+float Umidade_N;
+void setup() {  
+  Serial.begin(115200);
+}
+
+void loop() {
+
+}
